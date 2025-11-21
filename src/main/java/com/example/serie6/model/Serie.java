339e4c7b80c87335935c3f5f8e54f0afa4fa3b01
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-21 11:50:50 -0500

    commit - Serie :.

--- src/main/java/com/example/serie6/model/Serie.java
+++ src/main/java/com/example/serie6/model/Serie.java
@@ -1,4 +1,11 @@
 package com.example.serie6.model;
 
 public class Serie {
+    public int calcular(int limite) {
+        int suma = 0;
+        for (int i = 0; i <= limite; i += 6) {
+            suma += i;
+        }
+        return suma;    
+    }
 }
